a560cae8ce9b561070705cac7effc0a8b72105d7
MohamedHashim <sawmido2000@gmail.com>
2018-12-15 05:43:45 +0200

    Publish the firebase data to the map

--- app/src/main/java/com/mohamedhashim/turboapp/MapActivity.kt
+++ app/src/main/java/com/mohamedhashim/turboapp/MapActivity.kt
@@ -3,7 +3,6 @@ package com.mohamedhashim.turboapp
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.util.Log
-import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
@@ -13,7 +12,7 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.database.*
-import org.w3c.dom.Comment
+
 
 class MapActivity : FragmentActivity(), OnMapReadyCallback {
 
@@ -21,7 +20,8 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
     private var mapView: MapView? = null
     private val MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey"
     private lateinit var database: DatabaseReference
-    private var TAG ="LOGS_FIREBASE"
+    private lateinit var ref: DatabaseReference
+    private var TAG = "LOGS_FIREBASE"
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
@@ -41,31 +41,36 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         initMapView(savedInstanceState)
 
         database = FirebaseDatabase.getInstance().reference
+        ref = database.child("bins")
 //        writeNewUser("1","Vodafone",30.073521799999998,31.0183642,50.0f)
 //        writeNewUser("2","IBM Egypt",30.076902,31.025026,50.0f)
 //        writeNewUser("3","Microsoft",30.071118,31.016743,50.0f)
 //        writeNewUser("4","Valeo",30.078768,31.017816,50.0f)
 //        writeNewUser("5","Raya",30.074312,31.019999,50.0f)
 
-
-
     }
 
     override fun onMapReady(googleMap: GoogleMap?) {
-        val location = LatLng(30.073521799999998, 31.0183642)
-        val markerOptions = MarkerOptions()
-        markerOptions.position(location)
-            .icon(BitmapDescriptorFactory.fromResource(R.drawable.bin))
-            .title("Smart Village")
+        val phoneQuery = ref
+        phoneQuery.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                for (singleSnapshot in dataSnapshot.children) {
+                    val bin = singleSnapshot.getValue<Bin>(Bin::class.java)
+                    addMarker(bin!!, googleMap)
+                }
+            }
 
-        googleMap!!.addMarker(markerOptions)
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("logs_", "onCancelled", databaseError.toException())
+            }
+        })
         val camBuilder = CameraPosition.builder()
         camBuilder.bearing(45f)
         camBuilder.tilt(30f)
-        camBuilder.target(location)
+        camBuilder.target(LatLng(30.073521799999998, 31.0183642))
         camBuilder.zoom(15f)
         val cp: CameraPosition = camBuilder.build()
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cp))
+        googleMap!!.moveCamera(CameraUpdateFactory.newCameraPosition(cp))
     }
 
     private fun initMapView(savedInstanceState: Bundle?) {
@@ -112,8 +117,18 @@ class MapActivity : FragmentActivity(), OnMapReadyCallback {
         mapView!!.onLowMemory()
     }
 
-    private fun writeNewUser(id: String, name: String, lat: Double?, lng: Double?, status: Float?) {
+    private fun writeNewBin(id: String, name: String, lat: Double?, lng: Double?, status: Float?) {
         val bin = Bin(id, name, lat, lng, status)
-        database.child("bins").child(id).setValue(bin)
+        ref.child(id).setValue(bin)
+    }
+
+    private fun addMarker(bin: Bin, googleMap: GoogleMap?) {
+        val location = LatLng(bin.lat!!, bin.lng!!)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(location)
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.bin))
+            .title(bin.name + " " + bin.status + "%")
+
+        googleMap!!.addMarker(markerOptions)
     }
 }
